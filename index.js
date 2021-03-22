294d6f22fe6bb235a36d3c159e75954c687ebd5f
lea.arturi <lea.arturi@gmail.com>
2021-03-22 12:02:49 -0300

    Habilito CORS

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const conectarDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
 conectarDB();
+
+// Habilitar CORS
+app.use(cors());
 
 app.use(express.json({ extended: true }))
 
